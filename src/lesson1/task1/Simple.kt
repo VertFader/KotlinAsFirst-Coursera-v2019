ba37969cd067d7756a8cace29549b51ab6b29e69
VertFader <vitalychimrov@gmail.com>
2020-04-27 00:47:48 +0300

    Lesson 1. Task 1.Решены задачи на радиану и вычисление 3 цифры числа больше 100

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -30,8 +30,9 @@ fun discriminant(a: Double, b: Double, c: Double) = sqr(b) - 4 * a * c
  *
  * Поиск одного из корней квадратного уравнения
  */
-fun quadraticEquationRoot(a: Double, b: Double, c: Double) =
-    (-b + sqrt(discriminant(a, b, c))) / (2 * a)
+fun quadraticEquationRoot(a: Double, b: Double, c: Double): Double {
+    return (-b + sqrt(discriminant(a, b, c))) / (2 * a)
+}
 
 /**
  * Пример
@@ -49,10 +50,10 @@ fun quadraticRootProduct(a: Double, b: Double, c: Double): Double {
  * Пример главной функции
  */
 fun main() {
-    val x1x2 = quadraticRootProduct(1.0, 13.0, 42.0)
-    println("Root product: $x1x2")
+    val result = sqr(7)
+    println("7 * 7 = $result")
 }
-
+// result = 49 --> 7 * 7 = 49 (?)
 /**
  * Тривиальная
  *
@@ -76,7 +77,12 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(deg: Int, min: Int, sec: Int): Double = TODO()
+fun angleInRadian(deg: Int, min: Int, sec: Int): Double {
+    val degrees = deg + min/60.0 + sec/60.0/60.0
+    val partOfCircle = degrees / 360.0
+    val radian = partOfCircle * PI * 2.0
+    return radian
+}
 
 /**
  * Тривиальная
@@ -92,7 +98,8 @@ fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = TODO()
  * Пользователь задает целое число, большее 100 (например, 3801).
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
-fun thirdDigit(number: Int): Int = TODO()
+fun thirdDigit(number: Int): Int = number/100 % 10
+
 
 /**
  * Простая
